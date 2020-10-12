c3e1af8bed9df842b134a9874f6966c54c4b3c73
casual_degenerate <46980628+casualdegenerate@users.noreply.github.com>
2020-10-11 22:52:32 -0500

    Update Server Status.lua
    checks on if you are running proto

--- Server Status.lua
+++ Server Status.lua
@@ -1,3 +1,5 @@
+if syn_checkcaller then print("I do not have a version of this for SynX.") return end --Only works on proto
+if not is_protosmasher_caller then print("Only works on Proto") return end --Only works on proto
 while true do 
 	local h = game:HttpGet("https://www.roblox.com/games/getgameinstancesjson?placeId="..tostring(game.PlaceId).."&startIndex=0")
 	local j = game:GetService("HttpService"):JSONDecode(h)
