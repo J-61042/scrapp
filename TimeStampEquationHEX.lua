0190f95d8b4eb5f805d5867bdc63f113ad23cfd7
casual_degenerate <46980628+casualdegenerate@users.noreply.github.com>
2021-01-13 17:52:14 -0500

    Update TimeStampEquationHEX.lua
    this is my current version of it as of now ;v thought of updating it

--- TimeStampEquationHEX.lua
+++ TimeStampEquationHEX.lua
@@ -1,17 +1,16 @@
 neg = true
-scale = 1
+if neg then scale = 1 else scale = 1000 end --[[added if statement because you only need two.]]--If you want to use neg change scale to 1 for it to loop around properly. hhh
 hours = 0
-minutes = 424242
-seconds = 48
+minutes = 34
+seconds = 00
 if not neg then
-    time = ((hours*60^2)+(minutes*60)+seconds)
+    time = ((hours*60^2)+(minutes*60)+seconds)print(time)
 else
-    time = ((hours*60^2)-(minutes*60)-seconds-4294967296) -- Last number is the FF FF FF FF to reset all the way around.
+    time = (4294967296-(hours*60^2)-(minutes*60)-seconds)print(time) -- Last number is the FF FF FF FF to reset all the way around.
 end
-
 equ = scale*time
 print(equ)
-ans = equ
-hex = string.format("%x",ans)
+hex = string.format("%x",equ)
+print(string.format("%x",scale):upper())
 print(hex:upper())
 print(hex:len()/2)
